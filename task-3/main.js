d8ffca1370974a31b64d613271548a7a863d67a1
Sarkhan <syako44@gmail.com>
2022-07-13 01:54:49 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,8 @@
-const prompt = require('prompt');
-prompt.start();
+function reversed(arr){
+    for ( let i = arr.length - 1; i >= 0 ; i--) {
+        console.log(arr[i]);
+    }
+}
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let arr = [92846,96841,33582,25998]
+reversed(arr)
