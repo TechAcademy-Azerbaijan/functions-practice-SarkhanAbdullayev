b3059ac6eeee8ef7f326c64ac31899522746aebd
Sarkhan <syako44@gmail.com>
2022-07-13 02:13:15 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,14 @@
-const prompt = require('prompt');
-prompt.start();
+function fibonachi(n){
+    let first = 1
+    let second = 1
+    let fibo;
+    for (let i = 2; i < n; i++) {
+        fibo = first + second
+        first = second
+        second = fibo
+    }
+    console.log(fibo);
+}
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let num = 6
+fibonachi(num)
