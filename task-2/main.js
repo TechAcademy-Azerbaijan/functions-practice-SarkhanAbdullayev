d7240a8403f5cd59b2b954c5eb958f073e2ae7eb
Sarkhan <syako44@gmail.com>
2022-07-13 01:36:14 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,21 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    var num = parseInt(result.input); // 9
+    function prime(n){
+    let res = "Yes"
+    for (let i = 2; i < n; i++) {
+        if(n % i == 0){
+            res = "No"
+            break;
+        }
+    }
+
+    console.log(res);
+}
+prime(num);
 });
