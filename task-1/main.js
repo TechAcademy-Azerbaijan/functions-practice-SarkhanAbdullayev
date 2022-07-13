181fbb9e688609de9f93b197861b234c9a7bb4f9
Sarkhan <syako44@gmail.com>
2022-07-14 03:43:41 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,10 @@
-const prompt = require('prompt');
-prompt.start();
+function price(n){
+    let first = parseInt( n / 100 )
+    let second = parseInt( n % 100 / 20)
+    let third = n % 120
 
+    console.log(first * 100 + second * 30 + third * 2);
+}
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let disc = 123
+price(disc);
