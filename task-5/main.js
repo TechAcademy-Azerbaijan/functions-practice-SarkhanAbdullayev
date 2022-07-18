0018df1981d0acac05159364d555ca42205d9af8
Sarkhan <syako44@gmail.com>
2022-07-19 02:17:54 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,13 +1,31 @@
-function maxnumber(arr){
-    let max = 0
-    for (let i = 0; i< arr.length; i++) {
-        if(arr[i] > max){
-            max = arr[i]
-        }
+function maxnumber(a,b,c,d){
+
+    if(a > b && a > c && a > d){
+        console.log(a);
     }
-    console.log(max);
+    else if(b > a && b > c && b > d){
+        console.log(b);
+    }
+    else if(c > a && c > b && c > d){
+        console.log(c);
+    }
+    else{
+        console.log(d);
+    }
 }
 
-let array = [1,2,3,4]
+maxnumber(7,5,6,4);
 
-maxnumber(array)
+// function maxnumber(arr){
+//     let max = 0
+//     for (let i = 0; i< arr.length; i++) {
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     console.log(max);
+// }
+
+// let array = [1,2,3,4]
+
+// maxnumber(array)
